2ccf2aeecaec6c8d4be3c20d7d274e3fe35204bc
chroth7 <chroth@me.com>
2021-12-30 16:14:51 +0100

    Add `restaurant`.

---
+++ rustbook/restaurant/src/lib.rs
@@ -0,0 +1,85 @@
+// #[cfg(test)]
+// mod tests {
+//     #[test]
+//     fn it_works() {
+//         let result = 2 + 2;
+//         assert_eq!(result, 4);
+//     }
+// }
+//
+// Moved front_of_house to other file
+mod front_of_house;
+pub use crate::front_of_house::hosting;
+//
+// mod front_of_house {
+//     pub mod hosting {
+//         pub fn add_to_waitlist() {}
+//
+//         fn seat_at_table() {}
+//     }
+//
+//     mod serving {
+//         fn take_order() {}
+//
+//         fn serve_order() {}
+//
+//         fn take_payment() {}
+//     }
+// }
+
+// relative path import
+// use self::front_of_house::hosting;
+// absolute path import
+// use crate::front_of_house::hosting;
+
+mod back_of_house {
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    fn fix_incorrect_order() {
+        cook_order();
+        super::serve_order();
+    }
+
+    fn cook_order() {}
+}
+
+pub fn eat_at_restaurant() {
+    // Absolute path
+    // crate::front_of_house::hosting::add_to_waitlist();
+    // after `use`:
+    hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // the following line won't compile if we uncomment it; we're not allowed
+    // meal.seasonal_fruit = String::from("strawberries");
+
+    // using a fully public enum
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+fn serve_order() {}
